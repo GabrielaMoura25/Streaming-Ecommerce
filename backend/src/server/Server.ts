a1df9898f94841dac32eef40370b19b7046ffa4c
RubensLFerreira <rubenslima000@gmail.com>
2024-02-13 20:12:50 -0300

    feat: added bcrypt, yup and status codes dependencies.

--- backend/src/server/Server.ts
+++ backend/src/server/Server.ts
@@ -1,13 +1,11 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import express from 'express';
+import router from './routes';
 
 const server = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-
-server.get('/', (_: Request, res: Response) => {
-  res.json({ message: 'Projeto inicializado!' });
-});
+server.use(router);
 
 export default server;
