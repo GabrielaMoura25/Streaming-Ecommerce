6fced4d9fc83aa60e90c46f3e8943cf8eaab4528
RubensLFerreira <rubenslima000@gmail.com>
2024-02-13 20:10:07 -0300

    refactor: fixing changes

--- backend/src/server/models/User.ts
+++ backend/src/server/models/User.ts
@@ -7,12 +7,12 @@ class User extends Model implements IUser {
   id!: string;
   name!: string;
   cpf!: string;
-  phone!: string
-  gender?: "hc" | "ht" | "mc" | "mt" | "o";
+  phone!: string;
+  gender!: 'hc' | 'ht' | 'mc' | 'mt' | 'o' | undefined;
   dueDate!: string;
   email!: string;
   password!: string;
-  role!: "admin" | "client";
+  role!: 'admin' | 'user' | undefined;
 }
 
 User.init({
