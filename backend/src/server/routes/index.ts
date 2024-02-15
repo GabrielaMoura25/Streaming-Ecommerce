ae83fad2ad68dbcc8c89a1cb0a5bf0625a84a130
christianebs <christianebs90@gmail.com>
2024-02-15 15:06:18 -0300

    feat: inicialização da rota de streaming na index de rotas

--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router, Request, Response } from 'express';
 
 import userRoutes from './user.routes';
+import streamingRoutes from './streaming.routes';
+
 import { StatusCodes } from 'http-status-codes';
 
 const router: Router = Router();
 
 router.use('/user', userRoutes);
+router.use('/streaming', streamingRoutes);
 
 router.use('/', (_: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ message: 'Hello world!' });
