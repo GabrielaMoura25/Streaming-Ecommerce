9aa5ea106997a812d8b7bea1b445ba4045b870b7
christianebs <christianebs90@gmail.com>
2024-02-16 13:38:21 -0300

    feat: criação da rota de update

--- backend/src/server/routes/streaming.routes.ts
+++ backend/src/server/routes/streaming.routes.ts
@@ -1,9 +1,14 @@
-import { checkAdminRole } from './../middleware/checkAdminRole';
 import { Router } from 'express';
-import { createStreaming, getAllStreamings } from '../controllers/Streaming';
+
+import { createStreaming, getAllStreamings, updateStreamingById } from '../controllers/Streaming';
+
+import { checkAdminRole } from './../middleware/checkAdminRole';
 
 const router: Router = Router();
 
 router.route('/').post(checkAdminRole, createStreaming).get(getAllStreamings);
+
+router.route('/:id')
+  .put(checkAdminRole, updateStreamingById);
 
 export default router;
