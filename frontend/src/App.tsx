6f36fff29626664734ce3e44d384945a3d446e3e
leidyolinto <suverleide.olinto@gmail.com>
2024-02-18 11:34:38 -0300

    atualização do footer, instalação do styled

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./views/Home"
+import Home from "./views/Home";
+import Footer from './components/footer/Footer';
 
 function App() {
 
   return (
     <>
       <Home/>
+      <Footer/>
     </>
   )
 }
--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,16 +1,59 @@
-import "./footer.css";
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+ font-family: "Amiko", sans-serif;
+ font-size:bold;
+  background-color:  #333;
+  padding: 20px;
+  text-align: center;
+`;
+
+const StreamingEco = styled.div`
+  color: white;;
+  h3 {
+    font-size: 1.5em;
+  }
+`;
+const TeamInfo = styled.div`
+  color: white;
+  margin-top: 10px;
+  p {
+    margin: 0;
+  }
+`;
+const RepositoryLink = styled.a`
+  color:  white;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+const ResponsibleLink = styled.a`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 function Footer() {
   return (
-    <>
-      <footer className="footerDiv">
-        <div className="stramingEco"> 
-          <h3>2024 | Streaming Ecommerce ADA</h3>
-          </div>
-          <div><p>Rubéns | Jessy | Cristiane | Gabriela | Leidy</p></div>
-          <a href="">Repositório do Projeto</a>
-      </footer>
-    </>
+
+    <FooterContainer>
+      <StreamingEco>
+        <h3>2024 | Streaming Ecommerce ADA</h3>
+      </StreamingEco>
+      <TeamInfo>
+        <ResponsibleLink href="https://github.com/RubensLFerreira"> Rubéns Lima |</ResponsibleLink>
+        <ResponsibleLink href="https://github.com/JessanyKaline"> Jessany Kaline |</ResponsibleLink>
+        <ResponsibleLink href="https://github.com/christianebs"> Christiane Barbosa |</ResponsibleLink>
+        <ResponsibleLink href="https://github.com/GabrielaMoura25"> Gabriela Moura |</ResponsibleLink>
+        <ResponsibleLink href="https://github.com/LeidyOlinto"> Leidy Olinto |</ResponsibleLink>
+    
+      </TeamInfo>
+      <RepositoryLink href="">Repositório do Projeto</RepositoryLink>
+    </FooterContainer>
+     
   );
 }
 
