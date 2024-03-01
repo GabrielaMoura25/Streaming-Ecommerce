2cbc4e5895fce2de875138db3f330fed7d11bdeb
RubensLFerreira <rubenslima000@gmail.com>
2024-03-01 16:08:18 -0300

    refactor: upload file .env

--- frontend/src/components/ImagePromotion/index.tsx
+++ frontend/src/components/ImagePromotion/index.tsx
@@ -3,5 +3,5 @@ import Image from "react-bootstrap/Image";
 import promotion from "../../assets/images/promotion.png";
 
 export default function ImagePromotion() {
-	return <Image style={{ marginTop: "2rem" }} src={promotion} fluid />;
+	return <Image style={{ marginTop: "2rem", width: '100%' }} src={promotion} fluid />;
 }
